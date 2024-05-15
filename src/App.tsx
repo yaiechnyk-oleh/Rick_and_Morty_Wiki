5f8cd6097ddb9333aea2019e7a71b1f0fbd93340
yaiechnyk-oleh <kindercorporation13@gmail.com>
2024-05-16 01:42:41 +0300

    Created demo of project, beautified styles.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from "./Header/Header";
-import Dashboard from "./Dashbord/Dashbord";
+import Dashboard from "./Dashboard/Dashbord";
 
 function App() {
-
   const [filter, setFilter] = useState('');
   const [sort, setSort] = useState('');
+  const [reset, setReset] = useState(false);
+
+  useEffect(() => {
+    if (reset) {
+      setFilter('');
+      setSort('');
+      setReset(false);
+    }
+  }, [reset]);
 
   return (
-    <main className="App">
-    <Header setFilter={setFilter} setSort={setSort}/>
-    <Dashboard />
-    </main>
+      <main className="App">
+        <Header setFilter={setFilter} setSort={setSort} setReset={setReset} />
+        <Dashboard filter={filter} sort={sort} reset={reset} />
+      </main>
   );
 }
 
